c342b0dd70a8eed8883597120ef7bd4060e6e594
saro-farzamnia <sarofarzamnia0@gmail.com>
2025-01-15 23:51:46 -0800

    function

---
+++ script.js
@@ -0,0 +1,135 @@
+// "use strict"
+
+// Function (parameter)
+
+// function showMessage(){
+//     console.log("Hello Wrold!")
+// }
+// showMessage();
+
+// function dublicate(num){
+//     const result=num*2;
+//     console.log(result)
+// }
+// (arguement)
+// dublicate(3);
+
+// function sum(num1,num2){
+//     const result=num1+num2;
+//     console.log(result)
+// }
+// sum(2,5)
+
+
+//         // return
+// function sum(num1,num2){
+//     const result=num1+num2;
+//     console.log(result)
+//     return result;
+// }
+// console.log(sum(2,5))
+
+// const sumNumbers=sum(2,5);
+// console.log(sumNumbers);
+
+
+// Global scope
+// Local scope //function scope
+// Block scope //ES6
+
+// let name="Global";
+// function harchi(){
+//     let age=15
+//     console.log(age)
+// }
+// harchi()
+// if(true){ console.log("here is Block scope")
+//     let name="Global"
+// }
+// console.log(name)
+
+
+// var , let , const 
+// what the problem of var???
+
+// var name="benita";
+// console.log(name)
+
+// var name="saro";
+// console.log(name)
+
+// let lastName="farzamnia";
+// console.log(lastName);
+
+// let lastName="mohammadi";
+// console.log(lastName);
+
+// const age="15";
+// console.log(age)
+
+// const age="10";
+// console.log(age)
+
+
+// var , let , const in scopes
+
+// function age(){
+//     var num=2+1;
+//     console.log(num)
+// }
+// console.log(num)
+// age()
+
+// if(true){
+//     var name="benita"
+//     let age=10
+// }
+// console.log(name);
+// console.log(age);
+
+
+        // Hoisting
+
+// function
+// var - let - const 
+
+// harchi()
+
+// function harchi(){
+//     console.log("harchi")
+// }
+// console.log(name)
+// let name="harchi";
+// console.log(age)
+// var age=15;
+
+
+// strict mod 
+// "use strict"
+// number=5
+// console.log(number)
+// undefined=5
+// null=5
+
+
+// Function declareation
+// function sayMyName(name){
+//     console.log(`Your name is ${name}`)
+// }
+// sayMyName("Benita")
+
+
+// Function Expression
+// const funExpression=function(){
+//     console.log("Hello")
+// }
+// funExpression()
+
+
+// Anonymous Function
+// function (){console.log("Hello")}
+
+// IIFE --Immediately Invoked Function Expreesion
+// (function(){
+//     console.log("Hello IIFE")
+// }())
